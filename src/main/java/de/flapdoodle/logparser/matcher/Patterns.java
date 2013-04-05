ec8ec5f5eeeb7f803f103821b6f8ce3be36848fa
Michael Mosmann <michael@mosmann.de>
2013-04-05 16:44:25 +0200

    pattern hack

---
+++ src/main/java/de/flapdoodle/logparser/matcher/Patterns.java
@@ -0,0 +1,113 @@
+package de.flapdoodle.logparser.matcher;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.google.common.base.Function;
+import com.google.common.base.Optional;
+import com.google.common.collect.Collections2;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
+public class Patterns {
+
+	static final Method PATTERN_NAMED_GROUP_METHOD;
+	static {
+		try {
+			PATTERN_NAMED_GROUP_METHOD = Pattern.class.getDeclaredMethod("namedGroups");
+			PATTERN_NAMED_GROUP_METHOD.setAccessible(true);
+		} catch (NoSuchMethodException | SecurityException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private Patterns() {
+		// no instance
+	}
+
+	private static Map<String, Integer> callProtectedNamedGroupsMethod(Pattern pattern) {
+		try {
+			return (Map<String, Integer>) PATTERN_NAMED_GROUP_METHOD.invoke(pattern);
+		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static Set<String> names(Pattern pattern) {
+		return ImmutableSet.copyOf(callProtectedNamedGroupsMethod(pattern).keySet());
+	}
+
+	public static Optional<Map<String, String>> match(Matcher matcher) {
+		if (matcher.find()) {
+			Map<String, String> map = Maps.newHashMap();
+			for (String name : names(matcher.pattern())) {
+				map.put(name, matcher.group(name));
+			}
+			return Optional.of(map);
+		}
+		return Optional.absent();
+	}
+
+	public static Pattern build(Collection<String> parts) {
+		StringBuilder sb = new StringBuilder();
+		for (String part : parts) {
+			sb.append(part);
+		}
+		return Pattern.compile(sb.toString());
+	}
+
+	public static Pattern build(String... parts) {
+		return build(asCollection(parts));
+	}
+
+	public static Pattern join(Pattern... patterns) {
+		return join(asCollection(patterns));
+	}
+
+	public static Pattern join(Collection<Pattern> patterns) {
+		return build(asStrings(patterns));
+	}
+
+	public static Pattern namedGroup(String name, Pattern... patterns) {
+		return build(join(asCollection("(?<", name, ">"), asStrings(asCollection(patterns)), asCollection(")")));
+	}
+
+	private static Collection<String> asStrings(Collection<Pattern> patterns) {
+		return Collections2.transform(patterns, new Function<Pattern, String>() {
+
+			@Override
+			public String apply(Pattern input) {
+				return input.pattern();
+			}
+		});
+	}
+
+	private static <T> Collection<T> join(T before, Collection<T> collections, T... after) {
+		ArrayList<T> ret = Lists.newArrayList();
+		ret.add(before);
+		ret.addAll(collections);
+		for (T a : after) {
+			ret.add(a);
+		}
+		return ret;
+	}
+
+	private static <T> Collection<T> join(Collection<T>... collections) {
+		ArrayList<T> ret = Lists.newArrayList();
+		for (Collection<T> collection : collections) {
+			ret.addAll(collection);
+		}
+		return ret;
+	}
+
+	private static <T> Collection<T> asCollection(T... values) {
+		return Lists.newArrayList(values);
+	}
+}
--- src/test/java/de/flapdoodle/logparser/matcher/TestPatterns.java
+++ src/test/java/de/flapdoodle/logparser/matcher/TestPatterns.java
@@ -1,26 +1,56 @@
 package de.flapdoodle.logparser.matcher;
 
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import com.google.common.base.Optional;
 import com.google.common.collect.Lists;
-
 
 public class TestPatterns {
 
 	@Test
 	public void patternsWithGroups() {
-		Pattern pattern = Pattern.compile("(?<start>[a-zA-Z]+):(?<end>[a-zA-Z]+(?<number>[0-9]+))");
+		Pattern pattern = Patterns.build("(?<start>[a-zA-Z]+):(?<end>[a-zA-Z]+(?<number>[0-9]+))");
 		Matcher matcher = pattern.matcher("halli:hallo12");
-		if (matcher.find()) {
-			for (int i=0;i<=matcher.groupCount();i++){
-				System.out.println("Group("+i+")="+matcher.group(i));
-			}
-			for (String name : Lists.newArrayList("start","end","number")) {
-				System.out.println("Group("+name+")="+matcher.group(name));
-			}
-		}
+		
+		Optional<Map<String, String>> match = Patterns.match(matcher);
+		Assert.assertTrue(match.isPresent());
+		Assert.assertEquals("start", "halli", match.get().get("start"));
+		Assert.assertEquals("end", "hallo12", match.get().get("end"));
+		Assert.assertEquals("number", "12", match.get().get("number"));
+	}
+
+	@Test(expected = PatternSyntaxException.class)
+	public void groupNameCollision() {
+		Pattern pattern = Patterns.build("(?<bla>[a-z]+)", "\\s", "(?<bla>[a-z]+)");
+	}
+
+	@Test
+	public void groupNames() {
+		Pattern pattern = Patterns.build("(?<a>[a-z]+)", "\\s", "(?<b>[a-z]+)");
+		Matcher matcher = pattern.matcher("abc def");
+		Optional<Map<String, String>> match = Patterns.match(matcher);
+		Assert.assertTrue(match.isPresent());
+		Assert.assertEquals("a", "abc", match.get().get("a"));
+		Assert.assertEquals("b", "def", match.get().get("b"));
+	}
+
+	@Test
+	public void namedGroup() {
+		Pattern wordPattern = Patterns.build("[\\w]+");
+		Pattern namePattern = Patterns.join(Patterns.namedGroup("firstname", wordPattern), Patterns.build("\\s"),
+				Patterns.namedGroup("secondname", wordPattern));
+		
+		Matcher matcher = namePattern.matcher("Susi Sauerbraten");
+		Optional<Map<String, String>> match = Patterns.match(matcher);
+		
+		Assert.assertTrue("Match",match.isPresent());
+		Assert.assertEquals("firstname", "Susi", match.get().get("firstname"));
+		Assert.assertEquals("secondname", "Sauerbraten", match.get().get("secondname"));
 	}
 }
