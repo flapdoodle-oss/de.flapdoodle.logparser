2123ff8bda29ab77467e91141c838677ffbf567c
Michael Mosmann <michael@mosmann.de>
2013-04-07 15:26:38 +0200

    in progress with custom pattern

--- src/main/java/de/flapdoodle/logparser/matcher/CustomPatterns.java
+++ src/main/java/de/flapdoodle/logparser/matcher/CustomPatterns.java
@@ -1,5 +1,6 @@
 package de.flapdoodle.logparser.matcher;
 
+import java.util.logging.Level;
 import java.util.regex.Pattern;
 import static java.util.regex.Pattern.compile;
 
@@ -13,8 +14,37 @@ public class CustomPatterns {
 	public static final Pattern Package = build("[",SMALL_CAPS,"]+");
 	public static final Pattern Packages = join(Package, join(build("(", "\\.", Package.pattern(), ")*")));
 	public static final Pattern Classname = join(Packages,build("\\.[A-Z]","[",ALL_CAPS,"]*"));
+	public static final Pattern Method = join(Classname,build("\\.","[",ALL_CAPS,"]+"));
 			
+	public static final Pattern Date = compile("(?<date>([a-zA-Z]+) ([0-2]\\d|3[01]), (19|20|21)(\\d\\d) (1\\d|2\\d|\\d):([0-5][0-9]):([0-5][0-9]) (AM|PM))");
+	public static final Pattern Levels = compile(allLevels());
+	
 	private CustomPatterns() {
 		// no instance
 	}
+	
+	static String allLevels() {
+		StringBuilder sb=new StringBuilder();
+		sb.append("(");
+		appendLevel(sb, Level.CONFIG);
+		sb.append("|");
+		appendLevel(sb, Level.FINE);
+		sb.append("|");
+		appendLevel(sb, Level.FINER);
+		sb.append("|");
+		appendLevel(sb, Level.FINEST);
+		sb.append("|");
+		appendLevel(sb, Level.INFO);
+		sb.append("|");
+		appendLevel(sb, Level.SEVERE);
+		sb.append("|");
+		appendLevel(sb, Level.WARNING);
+		sb.append(")");
+		return sb.toString();
+	}
+	
+	static void appendLevel(StringBuilder sb, Level level) {
+		sb.append(level.getName());
+	}
+
 }
