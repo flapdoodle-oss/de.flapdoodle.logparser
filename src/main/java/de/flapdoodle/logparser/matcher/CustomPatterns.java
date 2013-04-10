d10afdfd62d87e563d60547bf4f1afa48c19f6b1
Michael Mosmann <michael@mosmann.de>
2013-04-10 09:18:44 +0200

    crazy class names

--- src/main/java/de/flapdoodle/logparser/matcher/CustomPatterns.java
+++ src/main/java/de/flapdoodle/logparser/matcher/CustomPatterns.java
@@ -34,7 +34,7 @@ public class CustomPatterns {
 	public static final Pattern Space = compile("\\s+");
 
 	public static final Pattern Method = compile("(\\<init\\>|[a-zA-Z][a-zA-Z0-9\\$]*)");
-	public static final Pattern Classname = compile("([a-zA-Z]+)((\\.|\\$)[a-zA-Z\\$][a-zA-Z\\$0-9]*)*");
+	public static final Pattern Classname = compile("([a-zA-Z\\$][a-zA-Z\\$0-9]*)((\\.|\\$)[a-zA-Z\\$][a-zA-Z\\$0-9]*)*");
 
 	public static final Pattern Date = compile("([a-zA-Z]+) ([0-2]\\d|3[01]), (19|20|21)(\\d\\d) (1\\d|2\\d|\\d):([0-5][0-9]):([0-5][0-9]) (AM|PM)");
 	public static final Pattern Levels = compile(allLevels());
